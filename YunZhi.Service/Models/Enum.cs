94174e5f26c87ab12a9a09451080159acd0d9503
Van <zheng_jinfan@126.com>
2020-09-27 23:25:14 +0800

    # Dockerfile added timezone to Shanghai

--- YunZhi.Service/Models/Enum.cs
+++ YunZhi.Service/Models/Enum.cs
@@ -22,27 +22,6 @@ namespace YunZhi.Service.Models
         Disabled = 2,
     }
     /// <summary>
-    /// 菜单类型
-    /// /// </summary>
-    public enum MenuType
-    {
-        /// <summary>
-        /// 目录
-        /// </summary>
-        [Description("目录")]
-        Catalog = 1,
-        /// <summary>
-        /// 菜单
-        /// </summary>
-        [Description("菜单")]
-        Menu = 2,
-        /// <summary>
-        /// 操作
-        /// </summary>
-        [Description("操作")]
-        Operate = 3
-    }
-    /// <summary>
     /// 权限类型
     /// </summary>
     public enum PermissionType
